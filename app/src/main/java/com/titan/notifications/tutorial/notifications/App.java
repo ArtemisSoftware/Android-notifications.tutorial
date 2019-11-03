25b9be126f4fb01b443f1d9b533f656fc70a2029
turim09 <gustavo.isaac@gmail.com>
2019-11-03 18:20:18 +0000

    Notification Channel Groups

--- app/src/main/java/com/titan/notifications/tutorial/notifications/App.java
+++ app/src/main/java/com/titan/notifications/tutorial/notifications/App.java
@@ -2,6 +2,7 @@ package com.titan.notifications.tutorial.notifications;
 
 import android.app.Application;
 import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.os.Build;
 
@@ -15,6 +16,13 @@ public class App extends Application {
     public static final String CHANNEL_6_ID = "channel6";
     public static final String CHANNEL_7_ID = "channel7";
 
+    public static final String GROUP_1_ID = "group1";
+    public static final String GROUP_2_ID = "group2";
+
+    public static final String CHANNEL_THREE_ID = "channelThree";
+    public static final String CHANNEL_FOUR_ID = "channelFour";
+
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -24,11 +32,29 @@ public class App extends Application {
 
     private void createNotificationChannels() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannelGroup group1 = new NotificationChannelGroup(GROUP_1_ID, "Group 1");
+            NotificationChannelGroup group2 = new NotificationChannelGroup(GROUP_2_ID, "Group 2");
+
+
             NotificationChannel channel1 = new NotificationChannel(CHANNEL_1_ID, "Channel 1", NotificationManager.IMPORTANCE_HIGH);
             channel1.setDescription("This is Channel 1");
+            channel1.setGroup(GROUP_1_ID);
 
             NotificationChannel channel2 = new NotificationChannel(CHANNEL_2_ID, "Channel 2", NotificationManager.IMPORTANCE_LOW);
             channel2.setDescription("This is Channel 2");
+            channel2.setGroup(GROUP_1_ID);
+
+
+            NotificationChannel channelThree = new NotificationChannel(CHANNEL_THREE_ID, "Channel Three", NotificationManager.IMPORTANCE_HIGH);
+            channelThree.setDescription("This is Channel three");
+            channelThree.setGroup(GROUP_2_ID);
+
+            NotificationChannel channelFour = new NotificationChannel(CHANNEL_FOUR_ID, "Channel Four", NotificationManager.IMPORTANCE_LOW);
+            channelFour.setDescription("This is Channel four");
+
+
+
 
             NotificationChannel channel3 = new NotificationChannel(CHANNEL_3_ID, "Channel 3", NotificationManager.IMPORTANCE_HIGH);
             channel3.setDescription("This is Channel 3");
@@ -45,9 +71,21 @@ public class App extends Application {
             NotificationChannel channel7 = new NotificationChannel(CHANNEL_7_ID, "Channel 7", NotificationManager.IMPORTANCE_LOW);
             channel7.setDescription("This is Channel 7");
 
+
+
+
+
             NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannelGroup(group1);
+            manager.createNotificationChannelGroup(group2);
+
             manager.createNotificationChannel(channel1);
             manager.createNotificationChannel(channel2);
+
+
+            manager.createNotificationChannel(channelThree);
+            manager.createNotificationChannel(channelFour);
+
             manager.createNotificationChannel(channel3);
             manager.createNotificationChannel(channel4);
             manager.createNotificationChannel(channel5);
