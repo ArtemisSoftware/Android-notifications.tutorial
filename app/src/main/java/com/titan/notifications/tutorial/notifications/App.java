81cc4702c05aa6563ef64e199292b15c7b563f88
turim09 <gustavo.isaac@gmail.com>
2019-11-03 16:30:40 +0000

    BigPictureStyle & MediaStyle

--- app/src/main/java/com/titan/notifications/tutorial/notifications/App.java
+++ app/src/main/java/com/titan/notifications/tutorial/notifications/App.java
@@ -9,6 +9,8 @@ public class App extends Application {
 
     public static final String CHANNEL_1_ID = "channel1";
     public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_3_ID = "channel3";
+    public static final String CHANNEL_4_ID = "channel4";
 
     @Override
     public void onCreate() {
@@ -25,9 +27,17 @@ public class App extends Application {
             NotificationChannel channel2 = new NotificationChannel(CHANNEL_2_ID, "Channel 2", NotificationManager.IMPORTANCE_LOW);
             channel2.setDescription("This is Channel 2");
 
+            NotificationChannel channel3 = new NotificationChannel(CHANNEL_3_ID, "Channel 3", NotificationManager.IMPORTANCE_HIGH);
+            channel1.setDescription("This is Channel 3");
+
+            NotificationChannel channel4 = new NotificationChannel(CHANNEL_4_ID, "Channel 4", NotificationManager.IMPORTANCE_LOW);
+            channel2.setDescription("This is Channel 4");
+
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel1);
             manager.createNotificationChannel(channel2);
+            manager.createNotificationChannel(channel3);
+            manager.createNotificationChannel(channel4);
         }
     }
 
--- app/src/main/java/com/titan/notifications/tutorial/notifications/NotificationsActivity.java
+++ app/src/main/java/com/titan/notifications/tutorial/notifications/NotificationsActivity.java
@@ -11,6 +11,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,10 @@ public class NotificationsActivity extends AppCompatActivity {
     private EditText editTextTitle;
     private EditText editTextMessage;
 
+    private MediaSessionCompat mediaSession;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +35,15 @@ public class NotificationsActivity extends AppCompatActivity {
 
         notificationManager = NotificationManagerCompat.from(this);
 
+        mediaSession = new MediaSessionCompat(this, "tag");
+
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextMessage = findViewById(R.id.edit_text_message);
 
         ((Button) findViewById(R.id.btn_sendOnChannel1)).setOnClickListener(btn_sendOnChannel1_OnClickListener);
         ((Button) findViewById(R.id.btn_sendOnChannel2)).setOnClickListener(btn_sendOnChannel2_OnClickListener);
+        ((Button) findViewById(R.id.btn_sendOnChannel3)).setOnClickListener(btn_sendOnChannel3_OnClickListener);
+        ((Button) findViewById(R.id.btn_sendOnChannel4)).setOnClickListener(btn_sendOnChannel4_OnClickListener);
     }
 
 
@@ -86,6 +95,8 @@ public class NotificationsActivity extends AppCompatActivity {
             String title = editTextTitle.getText().toString();
             String message = editTextMessage.getText().toString();
 
+            Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.audrey_hepburn_2);
+
             Notification notification = new NotificationCompat.Builder(getApplicationContext(), App.CHANNEL_2_ID)
                     .setSmallIcon(R.drawable.ic_two)
                     .setContentTitle(title)
@@ -95,6 +106,69 @@ public class NotificationsActivity extends AppCompatActivity {
                         .addLine("Sean Hepburn Ferrer")
                         .setBigContentTitle("Audrey Hepburn filhos")
                         .setSummaryText("Familia"))
+                    .setPriority(NotificationCompat.PRIORITY_LOW)
+                    .build();
+
+            notificationManager.notify(2, notification);
+        }
+    };
+
+
+
+    Button.OnClickListener btn_sendOnChannel3_OnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+
+            String title = editTextTitle.getText().toString();
+            String message = editTextMessage.getText().toString();
+
+            Intent activityIntent = new Intent(getApplicationContext(), NotificationsActivity.class);
+            PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, activityIntent, 0);
+
+            Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.audrey_hepburn_2);
+
+            Notification notification = new NotificationCompat.Builder(getApplicationContext(), App.CHANNEL_3_ID)
+                    .setSmallIcon(R.drawable.ic_one)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setLargeIcon(picture)
+                    .setStyle(new NotificationCompat.BigPictureStyle()
+                            .bigPicture(picture)
+                            .bigLargeIcon(null))
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                    .setContentIntent(contentIntent)
+                    .setAutoCancel(true)
+                    .setOnlyAlertOnce(true)
+                    .build();
+
+            notificationManager.notify(1, notification);
+        }
+    };
+
+
+    Button.OnClickListener btn_sendOnChannel4_OnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+
+            String title = editTextTitle.getText().toString();
+            String message = editTextMessage.getText().toString();
+
+            Bitmap artwork = BitmapFactory.decodeResource(getResources(), R.drawable.audrey_hepburn);
+
+            Notification notification = new NotificationCompat.Builder(getApplicationContext(), App.CHANNEL_4_ID)
+                    .setSmallIcon(R.drawable.ic_two)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setLargeIcon(artwork)
+                    .addAction(R.drawable.ic_dislike, "Dislike", null)
+                    .addAction(R.drawable.ic_previous, "Previous", null)
+                    .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(0,1)
+                        .setMediaSession(mediaSession.getSessionToken()))
+                    .setSubText("My artwork")
                     .setPriority(NotificationCompat.PRIORITY_LOW)
                     .build();
 
