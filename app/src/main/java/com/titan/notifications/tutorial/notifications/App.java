1154dcfd4e44bf942cf4280e87314bc16e75d8a8
turim09 <gustavo.isaac@gmail.com>
2019-11-03 17:33:10 +0000

    MessagingStyle & Direct Reply

--- app/src/main/java/com/titan/notifications/tutorial/notifications/App.java
+++ app/src/main/java/com/titan/notifications/tutorial/notifications/App.java
@@ -11,6 +11,7 @@ public class App extends Application {
     public static final String CHANNEL_2_ID = "channel2";
     public static final String CHANNEL_3_ID = "channel3";
     public static final String CHANNEL_4_ID = "channel4";
+    public static final String CHANNEL_5_ID = "channel5";
 
     @Override
     public void onCreate() {
@@ -33,11 +34,15 @@ public class App extends Application {
             NotificationChannel channel4 = new NotificationChannel(CHANNEL_4_ID, "Channel 4", NotificationManager.IMPORTANCE_LOW);
             channel2.setDescription("This is Channel 4");
 
+            NotificationChannel channel5 = new NotificationChannel(CHANNEL_5_ID, "Channel 3", NotificationManager.IMPORTANCE_HIGH);
+            channel1.setDescription("This is Channel 5");
+
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel1);
             manager.createNotificationChannel(channel2);
             manager.createNotificationChannel(channel3);
             manager.createNotificationChannel(channel4);
+            manager.createNotificationChannel(channel5);
         }
     }
 
--- app/src/main/java/com/titan/notifications/tutorial/notifications/NotificationsActivity.java
+++ app/src/main/java/com/titan/notifications/tutorial/notifications/NotificationsActivity.java
@@ -3,13 +3,16 @@ package com.titan.notifications.tutorial.notifications;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.RemoteInput;
 
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
@@ -17,6 +20,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.titan.notifications.tutorial.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -26,6 +32,7 @@ public class NotificationsActivity extends AppCompatActivity {
 
     private MediaSessionCompat mediaSession;
 
+    static List<Message> MESSAGES = new ArrayList<>();
 
 
     @Override
@@ -36,6 +43,10 @@ public class NotificationsActivity extends AppCompatActivity {
         notificationManager = NotificationManagerCompat.from(this);
 
         mediaSession = new MediaSessionCompat(this, "tag");
+
+        MESSAGES.add(new Message("My Fair Lady", "Jim"));
+        MESSAGES.add(new Message("Great movie", null));
+        MESSAGES.add(new Message("Wait Until Dark", "Jim"));
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextMessage = findViewById(R.id.edit_text_message);
@@ -143,7 +154,7 @@ public class NotificationsActivity extends AppCompatActivity {
                     .setOnlyAlertOnce(true)
                     .build();
 
-            notificationManager.notify(1, notification);
+            notificationManager.notify(3, notification);
         }
     };
 
@@ -172,7 +183,72 @@ public class NotificationsActivity extends AppCompatActivity {
                     .setPriority(NotificationCompat.PRIORITY_LOW)
                     .build();
 
-            notificationManager.notify(2, notification);
+            notificationManager.notify(4, notification);
         }
     };
+
+
+
+    public void sendOnChannel5(View v) {
+        sendChannel5Notification(this);
+    }
+
+    public static void sendChannel5Notification(Context context) {
+        Intent activityIntent = new Intent(context, NotificationsActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,
+                0, activityIntent, 0);
+
+        RemoteInput remoteInput = new RemoteInput.Builder("key_text_reply")
+                .setLabel("Your answer...")
+                .build();
+
+        Intent replyIntent;
+        PendingIntent replyPendingIntent = null;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            replyIntent = new Intent(context, DirectReplyReceiver.class);
+            replyPendingIntent = PendingIntent.getBroadcast(context,
+                    0, replyIntent, 0);
+        } else {
+            //start chat activity instead (PendingIntent.getActivity)
+            //cancel notification with notificationManagerCompat.cancel(id)
+        }
+
+        NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
+                R.drawable.ic_reply,
+                "Reply",
+                replyPendingIntent
+        ).addRemoteInput(remoteInput).build();
+
+        NotificationCompat.MessagingStyle messagingStyle =
+                new NotificationCompat.MessagingStyle("Me");
+        messagingStyle.setConversationTitle("Group Chat");
+
+        for (Message chatMessage : MESSAGES) {
+            NotificationCompat.MessagingStyle.Message notificationMessage =
+                    new NotificationCompat.MessagingStyle.Message(
+                            chatMessage.getText(),
+                            chatMessage.getTimestamp(),
+                            chatMessage.getSender()
+                    );
+            messagingStyle.addMessage(notificationMessage);
+        }
+
+        Notification notification = new NotificationCompat.Builder(context, App.CHANNEL_5_ID)
+                .setSmallIcon(R.drawable.ic_one)
+                .setStyle(messagingStyle)
+                .addAction(replyAction)
+                .setColor(Color.BLUE)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(5, notification);
+    }
+
+
 }
